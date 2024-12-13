680c1440353a74df549331e46a2434c0fd9779ed
flolgl <florianlegal@outlook.fr>
2024-12-13 15:19:02 +0100

    Added batch insert + and insert correction to not add already existing emails

--- python/contacts.py
+++ python/contacts.py
@@ -21,6 +21,8 @@ class Contacts:
               """
             )
             connection.commit()
+            cursor.execute("""CREATE UNIQUE INDEX index_contacts_email ON contacts(email)""")
+            connection.commit()
         self.connection = sqlite3.connect(db_path)
         self.connection.row_factory = sqlite3.Row
 
@@ -28,6 +30,7 @@ class Contacts:
         print("Inserting contacts ...")
         # TODO
         cursor = self.connection.cursor()
+        i = 0
         for contact in contacts:
             cursor.execute(
                 """
@@ -35,10 +38,12 @@ class Contacts:
                 """,
                 contact
             )
+            i += 1
+            if i == 10000:
+                i = 0
+                self.connection.commit()
 
         self.connection.commit()
-
-
 
     def get_name_for_email(self, email):
         print("Looking for email", email)
@@ -63,11 +68,19 @@ class Contacts:
         else:
             print("Not found")
 
+    def get_last_contact_id(self):
+        cursor = self.connection.cursor()
+        cursor.execute("""SELECT * FROM contacts ORDER BY id DESC LIMIT 1;""")
+        row = cursor.fetchone()
+        if row:
+            return row["id"]
+        return 0
 
-def yield_contacts(num_contacts):
+
+def yield_contacts(num_contacts, last_contact_id):
     # TODO: Generate a lot of contacts
     # instead of just 3
-    for i in range(1, num_contacts+1):
+    for i in range(last_contact_id+1, num_contacts + 1):
         yield (f"name-{i}", f"email-{i}@domain.tld")
 
 
@@ -75,7 +88,8 @@ def main():
     num_contacts = int(sys.argv[1])
     db_path = Path("contacts.sqlite3")
     contacts = Contacts(db_path)
-    contacts.insert_contacts(yield_contacts(num_contacts))
+    last_contact_id = contacts.get_last_contact_id()
+    contacts.insert_contacts(yield_contacts(last_contact_id+num_contacts, last_contact_id))
     charlie = contacts.get_name_for_email(f"email-{num_contacts}@domain.tld")
 
 
